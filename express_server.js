a62cb5e142909696dd9b4f6d17a011511e05b496
ahSOLO <junmenghan@gmail.com>
2021-01-19 18:06:32 -0800

    Started registration form

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.get("/urls/register", (req, res) => {
+  const templateVars = { username: req.cookies.username };
+  res.render("urls_register", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
